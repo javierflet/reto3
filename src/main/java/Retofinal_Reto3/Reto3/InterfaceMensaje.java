70e31f7b331e341bbe475e52d4773e58150e63ac
javierflet <javierflet@hotmail.com>
2021-11-17 19:58:03 -0500

    Correción
    Corregidos errores

---
+++ src/main/java/Retofinal_Reto3/Reto3/InterfaceMensaje.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Retofinal_Reto3.Reto3;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author mac
+ */  
+
+public interface InterfaceMensaje extends CrudRepository<Mensaje,Integer> {
+    
+} 
